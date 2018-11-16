4ce7184f1b84c2d943cfaa2a90131c785b06352a
K4123R <carlosguerra@k4123r.uconnect.utah.edu>
2018-11-16 13:08:28 -0700

    gcd and is three method added

---
+++ exe.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+g++ -std=c++14 -o execute execute.cpp
+./execute
+rm -f execute
---
+++ execute.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+
+using namespace std;
+
+//Function Declarations
+bool isThree(int a);
+int gcd(int a, int b);
+
+int main() {
+
+    cout << "/////////Is Three////////" << endl;
+    bool input = false;
+    cin >> input;
+    cout << input << endl;
+    cout << "////////////////////" << endl;
+
+
+    cout << "////////GCD/////////" << endl;
+    int a, b;
+    cout << "Input A: ";
+    cin >> a;
+    cout << "Input B: ";
+    cin >> b;
+    cout << "The GCD of " << a << " and " << b << " is " << gcd(a, b) << endl;
+    cout << "////////////////////" << endl;
+
+
+    return 0;
+}
+
+bool isThree(int a){
+    if(a == 3)
+        return true;
+    else {
+        return false;
+    }
+}
+
+//Calculates the GCD Recursively
+int gcd(int a, int b) {
+    if(b == 0)
+        return a;
+    else {
+        return gcd(b, a%b);
+    }    
+}
